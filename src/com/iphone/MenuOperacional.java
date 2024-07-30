3eeb3a78f5f92ce29a36bfd335f1d0ff9b6a72d3
romulo guilherme <rgouveiaconsultoria@hotmail.com>
2024-07-29 23:28:20 -0300

    segundo desafio

--- src/com/iphone/MenuOperacional.java
+++ src/com/iphone/MenuOperacional.java
@@ -3,5 +3,15 @@ package com.iphone;
 public class MenuOperacional {
     public static void main(String[] args) {
         Iphone iphone = new Iphone();
+        iphone.ligar("2195869586");
+        iphone.atender();
+        iphone.tocar();
+        iphone.pausar();
+        iphone.selecionarMusica("Paradise");
+        iphone.adicionarNovaAba();
+        iphone.exibirPagina("github");
+        iphone.iniciarCorreioVoz();
+        
+
     }
 }
